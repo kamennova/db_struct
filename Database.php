193c5633835358293145e270b31eb989096df8bc
Masha <mashushvik.2001@gmail.com>
2019-05-06 19:19:44 +0300

    +Properties, constructor

--- Database.php
+++ Database.php
@@ -1,12 +1,123 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: masha
- * Date: 04.05.19
- * Time: 19:50
- */
 
+require_once "BTree.php";
+require_once "FileEditor.php";
+
+/**
+ * Class Database
+ *
+ * Commands examples:
+ *
+ *  edit 12 'edited row', 14 => 'new edited row'
+ *  delete 12
+ *  insert 1 'new record', 23 'another record'
+ *
+ */
 class Database
 {
+    public $t;
+    public $tree;
+    public $filename;
 
+    /**
+     * @var array $format sets node and entry string structure = [
+     *      'node' => [
+     *          'flag' - ' '/e/d
+     *          'keys' - primary keys
+     *          [
+     *
+     * Example of node {
+     *          parent_pos: 56,
+     *          children_pos: [10, 345, 23, ],
+     *          keys: [1=>'data', 5=>'row',],
+     *          data_pos: ['100, 123, ]}
+     *
+     * node string: " .00056.00001.00005.     .00100.00123.     .00010.00345.00023.     .
+     *            d/e |parent| keys           | data pos        | children position     |
+     * entry: "010.{96 symbols}data"
+     *    data len| data
+     *
+     */
+    static public $format = [
+        'node' => [
+            'flag' => [
+                'length' => 1,
+            ],
+            'parent_pos' => [
+                'length' => 5
+            ],
+            'keys' => [
+                'quantity' => 0,
+                'length' => 5,
+            ],
+            'children_pos' => [
+                'quantity' => 0,
+                'length' => 5,
+            ],
+            'data' => [
+                'quantity' => 0,
+                'length' => '14',
+            ]
+        ],
+        'entry' => [
+            'data' => [
+                'length' => 100,
+            ]
+        ]
+    ];
+
+    static public $position_length = 5;
+    static public $empty_char = '|';
+
+    public $editor;
+
+    function __construct($filename, $t = 200, $data = null)
+    {
+        $this->filename = $filename;
+        $this->t = $t;
+        $this->editor = new FileEditor($this->filename);
+
+        if ($this->editor->size() === 0) {
+            $this->add_header();
+        }
+        return;
+
+        $this->tree = new BTree($this->t, $filename, $this->editor, true);
+
+        return;
+
+        if ($data) {
+            foreach ($data as $key => $value) {
+                $this->tree->insert($key, $value);
+            }
+        }
+
+        return;
+
+        echo "Commands format: {command_name} {key} (=>{value}";
+        readline('Enter a command: ');
+    }
+
+    function test()
+    {
+//        $this->spl->seek(1);
+        $this->spl->fwrite("                    88                       " . PHP_EOL);
+        $this->spl->fwrite("line22222222222222222222222222222222222222222" . PHP_EOL);
+        $this->spl->fwrite("line32222222222222222222222222222222222222222" . PHP_EOL);
+        $this->spl->fwrite("line42222222222222222222222222222222222222222" . PHP_EOL);
+
+        $this->spl->seek(0);
+        var_dump($this->spl->current());
+        $this->spl->fwrite("111111111111111111111111111111111111111111111\n");
+    }
+
+    function add_header()
+    {
+        $this->editor->first_write($this->t . "\n");
+    }
+
+    function insert($key, $data)
+    {
+        $this->tree->insert($key, $data);
+    }
 }
